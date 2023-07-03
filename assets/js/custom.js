f9a58e450975d3a637ff57af2fa7491f03835d97
Akbarputra04 <ap975527@gmail.com>
2023-07-03 20:00:11 +0700

    add detail, parallax, hover effect, & header menu type toggle

---
+++ assets/js/custom.js
@@ -0,0 +1,26 @@
+const menus = document.querySelectorAll('.topbar-menu .topbar-menu-link')
+const menuGroup = document.querySelectorAll('.header-menu .nav-menu')
+
+const menus2 = document.querySelectorAll('.canvas-action .action')
+const menuGroup2 = document.querySelectorAll('.canvas-menu .nav-menu')
+
+const filterMenu = (menus, menuGroup) => {
+  menus.forEach(menu => {
+    menu.addEventListener('click', e => {
+      e.preventDefault()
+      const dataType = e.target.dataset.type
+
+      menus.forEach(m => {
+        m.classList.remove('active')
+      })
+      e.target.classList.add('active')
+
+      menuGroup.forEach(product => {
+        product.classList.contains(dataType) ? product.classList.remove('d-none') : product.classList.add('d-none')
+      })
+    })
+  })
+}
+
+filterMenu(menus, menuGroup)
+filterMenu(menus2, menuGroup2)
